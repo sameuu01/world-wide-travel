c37d81e3e492e39fbbc1c0ccd9b75790ee0171d9
sameuu01 <samuel.clsmorais@gmail.com>
2026-06-04 20:14:50 -0300

    feat: terminando projeto

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,3 +1,31 @@
 function abrirMenu() {
     document.getElementById("menu").classList.toggle("show");
 }
+
+document.querySelectorAll(".carrossel").forEach(carrossel => {
+
+    const slides = carrossel.querySelectorAll(".slide");
+    const btnPrev = carrossel.querySelector(".esquerda");
+    const btnNext = carrossel.querySelector(".direita");
+
+    let slideAtual = 0;
+
+    function mostrarSlide(indice) {
+        slides.forEach(slide => {
+            slide.classList.remove("ativo");
+        });
+
+        slides[indice].classList.add("ativo");
+    }
+
+    btnNext.addEventListener("click", () => {
+        slideAtual = (slideAtual + 1) % slides.length;
+        mostrarSlide(slideAtual);
+    });
+
+    btnPrev.addEventListener("click", () => {
+        slideAtual = (slideAtual - 1 + slides.length) % slides.length;
+        mostrarSlide(slideAtual);
+    });
+
+});
